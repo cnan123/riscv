1a264e8f8d570bee2a8b9e3bbbb82838e2b9f553
cnan123 <15256267553@163.com>
2021-10-02 11:25:00 +0800

    rm unused file

--- rtl/bht.sv
+++ rtl/bht.sv
@@ -32,13 +32,30 @@ module bht#(
 //////////////////////////////////////////////
 //main code
 
-sat_counter sat_counter(/*AUTOINST*/
-			// Inputs
-			.clk		(clk),
-			.reset_n	(reset_n),
-			.update_en	(update_en),
-			.taken		(taken),
-			.predict_taken	(predict_taken));
+
+
+always_comb begin: update_bht
+    sat_counter_n = sat_counter_q;
+
+    if( update_en )begin
+        if( sat_counter_q[updata_pc][][1:0] == 2'b11 )begin
+            if(!taken)begin
+                sat_counter[1:0] <= sat_counter[1:0] - 1;
+            end
+        end else if( sat_counter[1:0] == 2'b00 ) begin
+            if(taken)begin
+                sat_counter[1:0] <= sat_counter[1:0] + 1;
+            end
+        end else begin
+            if( taken )begin
+                sat_counter[1:0] <= sat_counter[1:0] + 1;
+            end else begin
+                sat_counter[1:0] <= sat_counter[1:0] - 1;
+            end
+        end
+    end
+end
+
 
 
 endmodule
